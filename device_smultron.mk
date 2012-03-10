43dbd42c3ccd8cfc52232dd08ae32f689b05a1d9
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-10 17:16:54 +0000

    smultron: Use bootrec-device as device-specific init for the generic bootrec
    Change-Id: Id7051b47cd9f85d39c14d87020e4a2e773e919af

--- device_smultron.mk
+++ device_smultron.mk
@@ -29,7 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/smultron/overlay
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/smultron/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
-    device/semc/smultron/prebuilt/bootrec:root/sbin/bootrec \
+    device/semc/smultron/prebuilt/bootrec-device:root/sbin/bootrec-device \
     device/semc/smultron/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/smultron/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
