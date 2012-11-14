ed4f57535dad085a32cc9a353c097f51eb7aadc6
codeworkx <codeworkx@cyanogenmod.com>
2012-11-14 15:45:49 +0100

    smultron: device specific part for the new two-stage boot method
    Ported from http://review.cyanogenmod.org/#/c/26440/
    Author: codeworkx <codeworkx@cyanogenmod.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ic9ad9fb4f6d700ac469bbe193176425b5b5dd6bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,8 @@ TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/smultron/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
+
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
--- smultron.mk
+++ smultron.mk
@@ -19,9 +19,11 @@ PRODUCT_COPY_FILES += \
     device/semc/smultron/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/smultron/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/smultron/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/smultron/prebuilt/bootrec-device:root/sbin/bootrec-device \
-    device/semc/smultron/recovery.fstab:root/recovery.fstab
+    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle
+
+# Device specific part for two-stage boot
+PRODUCT_COPY_FILES += \
+    device/semc/smultron/recovery/bootrec-device:recovery/bootrec-device
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
