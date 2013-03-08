f280690f60be6691c7a733337b83731338eafdab
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 03:41:49 +0200

    smultron: some cleanup
    * Removed Phone overlay (mdpi fix is merged in Phone app)
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I80d21094c9138a95a61224a62cc04088d86c3b79

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,21 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/smultron/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
 
-#Sensors
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA250_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
 TARGET_OTA_ASSERT_DEVICE := ST15i,ST15a,smultron
 
-#Bluetooth
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/smultron/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/semc/smultron/bluetooth/vnd_smultron.txt
