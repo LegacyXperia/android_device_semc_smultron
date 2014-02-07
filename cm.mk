3a2921d3037d8131d3ccf4a1b4d59fabc558042c
Michael Bestas <mikeioannina@gmail.com>
2014-02-07 17:15:20 +0200

    smultron: Fix device name
    Change-Id: Ib7ab29ed81783c2c6c24375cde489823036c752c

--- cm.mk
+++ cm.mk
@@ -33,9 +33,12 @@ PRODUCT_PACKAGES += \
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=ST15i \
+    PRODUCT_DEVICE=ST15i \
     BUILD_FINGERPRINT="SEMC/ST15i_1254-2184/ST15i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
     PRIVATE_BUILD_DESC="ST15i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_smultron
-PRODUCT_DEVICE := smultron
+
+# Release name
+PRODUCT_RELEASE_NAME := smultron
