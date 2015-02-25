124f5eaa29c8601b50f2431d72e3b9e68a60de60
Michael Bestas <mikeioannina@gmail.com>
2015-02-25 03:01:04 +0200

    Fix overriding device name
    Change-Id: I0f61a3a54f88a2ef30bc41e6828e8feea861bb2a

--- cm.mk
+++ cm.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=ST15i \
-    PRODUCT_DEVICE=ST15i \
+    TARGET_DEVICE=ST15i \
     BUILD_FINGERPRINT="SEMC/ST15i_1254-2184/ST15i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
     PRIVATE_BUILD_DESC="ST15i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
